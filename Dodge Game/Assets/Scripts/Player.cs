95542fea87d2c7b932566ea458c3051bda7ac72f
jhyun1234 <anjyounl24@gmail.com>
2024-03-01 15:02:35 +0900

    2024.2.28 2D Project Dodge Game

---
+++ Dodge Game/Assets/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player: MonoBehaviour
+{
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] Vector2 direction;
+    [SerializeField] float speed = 1.0f;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        
+    }
+
+    
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+
+        if(direction.x <0)
+        {
+            animator.SetBool("Run", true);
+            spriteRenderer.flipX = false;
+        }
+        else if(direction.x>0)
+        {
+            animator.SetBool("Run" ,true) ;
+            spriteRenderer.flipX = true;
+        }
+
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
